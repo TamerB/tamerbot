66d7f7dd89103d8a6275bc40be4b3965ae620cf6
TamerB <tamer.bhgt@gmail.com>
2017-10-15 12:12:40 +0200

    Added test and timout to serviceRegistry

--- config/index.js
+++ config/index.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   wit_token: process.env.WIT_TOKEN,
   slack_token: process.env.SLACK_TOKEN,
-  slackLogLevel: 'verbose'
+  slackLogLevel: 'verbose',
+  serviceTimout: 30
 }
